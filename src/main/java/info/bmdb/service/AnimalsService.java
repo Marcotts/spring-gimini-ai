b58e7cab9c4e947b8164a98f8f9e5ce66beecf40
Marcotts <ccdsergio@gmail.com>
2026-02-21 12:11:21 -0500

    Ajuter le tool get time date

--- src/main/java/info/bmdb/service/AnimalsService.java
+++ src/main/java/info/bmdb/service/AnimalsService.java
@@ -6,10 +6,10 @@ import org.springframework.ai.tool.annotation.ToolParam;
 public class AnimalsService {
 
     @Tool(description = "Mais ou est le poisson")
-    public String chercheunpoisson(@ToolParam(description= "Comment s'appel le poisson si pas nommé alors appel le Audrey") String location) {
+    public String chercheunpoisson(@ToolParam(description= "Comment s'appel le poisson si pas nommé alors nomme le Audrey") String location) {
 
         if (location == null) {return "Sergio";}
 
-        return "New York";
+        return "New York est l'endoit ou est le poisson.";
     }
 }
